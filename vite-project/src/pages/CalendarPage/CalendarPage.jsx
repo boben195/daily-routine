43c18105409be704752820b742debc449c9bbdbb
boben195 <boben195@ukr.net>
2025-04-23 22:27:36 +0300

    test

--- vite-project/src/pages/CalendarPage/CalendarPage.jsx
+++ vite-project/src/pages/CalendarPage/CalendarPage.jsx
@@ -1,38 +1,38 @@
-import React, { useState } from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
-import { useNavigate } from "react-router-dom";
-import "./CalendarPage.css";
+// import React, { useState } from "react";
+// import Calendar from "react-calendar";
+// import "react-calendar/dist/Calendar.css";
+// import { useNavigate } from "react-router-dom";
+// import "./CalendarPage.css";
 
-const formatDateLocal = (dateObj) => {
-  const year = dateObj.getFullYear();
-  const month = String(dateObj.getMonth() + 1).padStart(2, "0");
-  const day = String(dateObj.getDate()).padStart(2, "0");
-  return `${year}-${month}-${day}`;
-};
+// const formatDateLocal = (dateObj) => {
+//   const year = dateObj.getFullYear();
+//   const month = String(dateObj.getMonth() + 1).padStart(2, "0");
+//   const day = String(dateObj.getDate()).padStart(2, "0");
+//   return `${year}-${month}-${day}`;
+// };
 
-const CalendarPage = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const navigate = useNavigate();
+// const CalendarPage = () => {
+//   const [selectedDate, setSelectedDate] = useState(new Date());
+//   const navigate = useNavigate();
 
-  const handleDateClick = (date) => {
-    const formatted = formatDateLocal(date);
-    navigate(`/day/${formatted}`);
-  };
+//   const handleDateClick = (date) => {
+//     const formatted = formatDateLocal(date);
+//     navigate(`/day/${formatted}`);
+//   };
 
-  return (
-    <div className="calendar-container">
-      <h1>🗓️ My Daily Routine</h1>
-      <Calendar
-        onChange={(date) => {
-          setSelectedDate(date);
-          handleDateClick(date);
-        }}
-        value={selectedDate}
-        locale="en-US"
-      />
-    </div>
-  );
-};
+//   return (
+//     <div className="calendar-container">
+//       <h1>🗓️ My Daily Routine</h1>
+//       <Calendar
+//         onChange={(date) => {
+//           setSelectedDate(date);
+//           handleDateClick(date);
+//         }}
+//         value={selectedDate}
+//         locale="en-US"
+//       />
+//     </div>
+//   );
+// };
 
-export default CalendarPage;
+// export default CalendarPage;
