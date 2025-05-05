c4fb2fb0cdcc3799fe466cb45033508ccd15b7a3
boben195 <boben195@ukr.net>
2025-05-05 20:58:46 +0300

    add style to dayview

--- vite-project/src/pages/DayViewPage/DayView.jsx
+++ vite-project/src/pages/DayViewPage/DayView.jsx
@@ -4,17 +4,28 @@ import DailyRoutine from "../../components/DailyRoutine/DailyRoutine";
 import TodoList from "../../components/ToDoList/ToDoList";
 import ExpenseTable from "../../components/Expence/ExpenseTable";
 
+import "./DayView.css";
+
 const DayView = () => {
   const { date } = useParams();
 
   const safeDate = decodeURIComponent(date);
   return (
     <div className="day-tasks">
-      <h1>Tasks for {date}</h1>
-      {/* Later: Load tasks from localStorage or database */}
-      <DailyRoutine />
-      <TodoList date={safeDate} />
-      <ExpenseTable />
+      <h1 className="day-title">Tasks for {safeDate}</h1>
+      <div className="day-layout">
+        <div className="left-column">
+          <DailyRoutine />
+        </div>
+        <div className="right-column">
+          <div className="todo-wrapper">
+            <TodoList date={safeDate} />
+          </div>
+          <div className="expense-wrapper">
+            <ExpenseTable date={safeDate} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
